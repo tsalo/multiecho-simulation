ee9890e89166c2ad4d4fb30aabf5048ae6194c8e
Taylor Salo <tsalo90@gmail.com>
2025-09-10 13:31:20 -0400

    Add sandbox notebook.

--- simlib/generate_timeseries.py
+++ simlib/generate_timeseries.py
@@ -1,4 +1,4 @@
-""" Generate time series with different characteristics to use as input to generate_sims at different echoes"""
+"""Generate time series with different characteristics to use as input to generate_sims at different echoes."""
 
 from typing import List
 
@@ -8,27 +8,31 @@ from scipy import signal
 
 
 def gen_randfreq_timeseries(n_reps: int, n_timepoints: int, n_freq: int) -> npt.NDArray:
-    """
-    time series that is a combination of sine waves with uniform random freq & phase and amplitude
+    """Generate time series that is a combination of sine waves with uniform random freq & phase and amplitude.
 
     This should be similar to band pass filtering random time series, but with a bit more control over frequencies
 
     Parameters
     ----------
-    n_reps: (int) Number of repetitions (voxels) to create time series for
-    n_timepoints: (int) Number of time points for each repetition
-    n_freq: (int) number of sin waves to
+    n_reps : :obj:`int`
+        Number of repetitions (voxels) to create time series for
+    n_timepoints: :obj:`int`
+        Number of time points for each repetition
+    n_freq: :obj:`int`
+        number of sin waves to add together
 
     Returns
     -------
     A 2D matrix n_reps time series of n_timepoints durations.
-    Each time series has is a combination of n_freq sinusoids each with a random frequency (between 1 cycle for the entire time course & 1 cycle every 5 timepoints),
+    Each time series has is a combination of n_freq sinusoids each with a random frequency 
+    (between 1 cycle for the entire time course & 1 cycle every 5 timepoints),
     a random phase, and a random amplitude (0.1-1.0).
     After the sinusoids are added together they are scaled to have a standard deviation of 1.
 
     Note
     ----
-    This function is used as one of several options for generating simulated time series and should be fairly similar to bandpass filtering random noise (another option)
+    This function is used as one of several options for generating simulated time series and should be 
+    fairly similar to bandpass filtering random noise (another option)
     """
 
     freq_random = np.zeros((n_reps, n_timepoints))
@@ -56,13 +60,14 @@ def gen_randfreq_timeseries(n_reps: int, n_timepoints: int, n_freq: int) -> npt.
 
 
 def gen_randn_timeseries(n_reps: int, n_vals: int) -> npt.NDArray:
-    """
-    time series that are Gaussian random noise
+    """Generate time series that are Gaussian random noise.
 
     Parameters
     ----------
-    n_reps: (int) Number of repetitions (voxels) to create time series for
-    n_timepoints: (int) Number of time points for each repetition
+    n_reps: :obj:`int`
+        Number of repetitions (voxels) to create time series for
+    n_timepoints: :obj:`int`
+        Number of time points for each repetition
 
     Returns
     -------
@@ -77,21 +82,23 @@ def gen_bandpass_randn_timeseries(
     passband: List[float] = [1 / 100, 1 / 20],
     fs: float = 0.5,
 ) -> npt.NDArray:
-    """
-    time series that are Gaussian random noise and then bandpass filtered
+    """Generate time series that are Gaussian random noise and then bandpass filtered.
 
     Parameters
     ----------
-    n_reps: (int) Number of repetitions (voxels) to create time series for
-    n_timepoints: (int) Number of time points for each repetition
-    passband: List(float) Two numbers that are the top and bottom frequencies of the pass band
-    fs: (float) The sampling frequency of the digital system
+    n_reps: :obj:`int`
+        Number of repetitions (voxels) to create time series for
+    n_timepoints: :obj:`int`
+        Number of time points for each repetition
+    passband: :obj:`List[float]`
+        Two numbers that are the top and bottom frequencies of the pass band
+    fs: :obj:`float`
+        The sampling frequency of the digital system
 
     Returns
     -------
     A 2D matrix n_reps time series of n_timepoints durations of Gaussian random noise
     that is then bandpass filtered with the passband
     """
-
     sos = signal.butter(10, passband, "bandpass", fs=fs, output="sos")
     return signal.sosfilt(sos, gen_randn_timeseries(n_reps, n_timepoints), axis=1)
